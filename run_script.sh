f1b98d2e0ffaad92ce3a2f739b0bbf0ad0982b19
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-10-04 01:54:13 +0200

    configuration variable name et mail

--- run_script.sh
+++ run_script.sh
@@ -2,6 +2,8 @@ git clone https://github.com/ilan990/Tmdb.git
 ls
 cd Tmdb
 python script.py
+git config --global user.email "$GIT_MAIL"
+git config --global user.name "$GIT_NAME"
 git add .
 git commit -m "MAJ automatique"
 git push origin master
