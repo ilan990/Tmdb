756c88a98a14188376516198d98b66757e45d426
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-10-04 18:08:51 +0200

    réparation bug et suppressions des tests

--- run_script.sh
+++ run_script.sh
@@ -1,11 +1,12 @@
 git clone https://github.com/ilan990/Tmdb.git
+
 cd Tmdb
-ls -a
+
 git remote remove origin
 git remote add origin $GIT_TOKEN_URL
-touch top_rated_movies.json
-ls -a
+
 python script.py
+
 git config --global user.email "$GIT_MAIL"
 git config --global user.name "$GIT_NAME"
 git add .
