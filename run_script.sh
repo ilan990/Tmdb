ff6c290a992ac0fa427bc327736ba70932281a28
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-10-04 02:09:16 +0200

    test

--- run_script.sh
+++ run_script.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/ilan990/Tmdb.git
-ls
 cd Tmdb
+git remote remove origin
+git remote add origin $GIT_TOKEN_URL
 python script.py
 git config --global user.email "$GIT_MAIL"
 git config --global user.name "$GIT_NAME"
