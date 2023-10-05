b63e50638a5f730d9cdd5857269c271700131408
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-10-05 23:43:47 +0200

    envoi un sms que quand c'est un film de moins de 7 ans

--- script.py
+++ script.py
@@ -33,43 +33,55 @@ def load_movies_from_json(filename):
 
 def send_sms(message):
     try:
-        response = requests.get(os.environ.get('URL') + f'user={os.environ.get("FREE_USER")}&pass={os.environ.get("FREE_API_KEY")}&msg={message}')
+        response = requests.get(os.environ.get(
+            'URL') + f'user={os.environ.get("FREE_USER")}&pass={os.environ.get("FREE_API_KEY")}&msg={message}')
         if response.status_code == 200:
             print('Le message a bien été envoyé')
         else:
             print('Échec de l\'envoi')
     except Exception as e:
-        print(f'Une erreur est survenue : {e}')
+        print(f'Une erreur est survenue: {e}')
 
 
 def main():
-    # Remplacez 'VOTRE_CLE_API' par votre clé d'API TMDb
     api_key = os.environ.get('TMDB_API_KEY')
+
+    # Obtenir l'année actuelle
+    current_year = datetime.now().year
 
     # Obtenir les nouveaux films
     new_movies = get_top_rated_movies(api_key)
+
+    # Filtrer les nouveaux films sortis au cours des 7 dernières années
+    new_movies_filtered = [film for film in new_movies if current_year - int(film['release_date'][:4]) <= 7]
 
     # Charger les anciens films depuis le fichier JSON
     old_movies = load_movies_from_json('top_rated_movies.json')
 
     # Identifier les nouveaux films
-    new_movie_ids = set([film['id'] for film in new_movies])
+    new_movie_ids = set([film['id'] for film in new_movies_filtered])
     old_movie_ids = set([film['id'] for film in old_movies])
     new_ids = new_movie_ids - old_movie_ids
 
     # Obtenir les détails des nouveaux films
-    new_movies_data = [film for film in new_movies if film['id'] in new_ids]
+    new_movies_data = [film for film in new_movies_filtered if film['id'] in new_ids]
 
-    # Liste pour stocker les titres des nouveaux films
-    new_movie_titles = [film['title'] for film in new_movies_data]
+    # Créer le message avec les titres et les dates des nouveaux films
+    message_lines = []
+    for film in new_movies_data:
+        release_year = int(film['release_date'][:4])
+        message_line = f"{film['title']} ({release_year})"
+        message_lines.append(message_line)
 
-    # Envoyer un SMS avec les titres des nouveaux films
-    if new_movies_data:
-        message = f"Coucou c'est IlanGPT, il y a du nouveau dans le top des films: {', '.join(new_movie_titles)}"
+    # Envoyer un SMS avec les titres et les dates des nouveaux films si au moins un film est trouvé
+    if message_lines:
+        message = f"Coucou c'est IlanGPT, voici les nouveaux films sortis au cours des 7 dernières années : \n\n"
+        message += "\n".join(message_lines)
         send_sms(message)
 
     # Enregistrer les nouveaux films dans un fichier JSON
     save_movies_to_json(new_movies, 'top_rated_movies.json')
+
 
 if __name__ == '__main__':
     main()
